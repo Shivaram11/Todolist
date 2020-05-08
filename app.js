9c050eebf2e0a689dacfc0a4678d3351dad9ecaf
shivaram <shivaramkumar2000@gmail.com>
2020-05-08 14:07:49 +0530

    to do list is ready

---
+++ app.js
@@ -0,0 +1,46 @@
+//selectors
+const toDoInput = document.querySelector(".to-do-input");
+const toDoButton =document.querySelector(".to-do-button");
+const toDoList= document.querySelector(".to-do-list");
+
+//listeners
+toDoButton.addEventListener("click",addToDO);
+toDoList.addEventListener("click",addRemove);
+
+//functions
+ function addToDO(event){
+  event.preventDefault();
+  console.log("hey now brwon cow");
+  const toDoDiv= document.createElement("div");
+  toDoDiv.classList.add("to-do");
+
+  const newToDO=document.createElement("li");
+  newToDO.innerText=toDoInput.value;
+  newToDO.classList.add("to-do-item");
+  toDoDiv.appendChild(newToDO);
+
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  toDoDiv.appendChild(completedButton);
+
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML='<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete-btn");
+  toDoDiv.appendChild(deleteButton);
+
+  toDoList.appendChild(toDoDiv);
+  toDoInput.value="";
+ }
+function addRemove(event){
+  const item=event.target;
+  console.log(item);
+  if(item.classList[0]=="delete-btn"){
+    item.parentElement.remove();
+
+  }
+  if(item.classList[0]=="complete-btn"){
+    item.parentElement.classList.add("completed");
+  }
+}
